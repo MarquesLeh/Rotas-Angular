ac8a5ec22583fc8f96c855ae8480181e649b3f60
PedroSpiet <pedro.silva940@etec.sp.gov.br>
2019-10-25 16:39:44 -0300

    Criando e dando export e import no  modulo pagina,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { PaginasModule } from './paginas';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PaginasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
